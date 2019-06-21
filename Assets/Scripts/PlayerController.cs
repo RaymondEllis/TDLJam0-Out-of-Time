c572a8c3f0343bf30bd81a4de4fa6eee490e7ed3
Raymond Ellis <raymondellis89@gmail.com>
2019-06-20 21:42:23 -0700

    Basic 2D platform control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float Power;
+	public float JetPower;
+
+	private Rigidbody2D rb;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		var move = Input.GetAxis("Horizontal") * Power;
+		rb.AddForce(Vector2.right * move * Time.fixedDeltaTime);
+
+
+		var jet = Input.GetAxis("Vertical");
+		if (jet > 0f)
+			rb.AddForce(Vector2.up * jet * JetPower * Time.fixedDeltaTime);
+	}
+}
