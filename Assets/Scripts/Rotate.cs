32bb2c53ed807dfdb76d60793a5382cf8e214b9e
Raymond Ellis <raymondellis89@gmail.com>
2019-06-21 06:17:55 -0700

    Basic exit

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	public float Speed;
+
+	private void FixedUpdate()
+	{
+		transform.Rotate(Vector3.forward, Speed * Time.fixedDeltaTime);
+	}
+}
